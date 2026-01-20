55bed6b5707275cbad07532000cd92c74d493b8a
adrien meinier <adrien.meinier@laplateforme.io>
2026-01-20 10:29:25 +0100

    separations fichiers

---
+++ settings.py
@@ -0,0 +1,12 @@
+import pygame
+
+SCREENWIDTH = 800
+SCREENHEIGHT = 640
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+GRAY = (80, 80, 80)
+
+pygame.font.init()
+FONT = pygame.font.SysFont("Comic Sans MS", 22)
